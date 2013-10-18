c2833feb46fe17b015849d38ddfd96357ab78fda
Michael Keplinger <michael@smashbrand.com>
2013-10-18 12:44:34 -0400

    initial commit

---
+++ login-tracker/index.php
@@ -0,0 +1,270 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <title>Crittenden | Home</title>
+    <meta name="author" content="SmashBrand">
+    <meta name="viewport" content="960px">
+    <link href="http://directory.crittendenonline.com/media/twitter-bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://directory.crittendenonline.com/media/twitter-bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="http://directory.crittendenonline.com/media/css/_global.css" rel="stylesheet">
+    <link href="http://directory.crittendenonline.com/media/css/page-index.css" rel="stylesheet">
+    <link href="http://directory.crittendenonline.com/media/js/subscriber/chosen/chosen/chosen.css" rel="stylesheet">
+
+    
+  <link href="http://directory.crittendenonline.com/media/css/page-login.css" rel="stylesheet">
+  <link href="http://directory.crittendenonline.com/media/css/page-login-top-level.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    	<style type="text/css">
+		.container-fluid {
+			max-width: 960px;
+			margin: 0 auto;
+		}
+	</style>
+    <![endif]-->
+
+    <!--[if gte IE 9]>
+      <style type="text/css">
+        .gradient {
+           filter: none;
+        }
+      </style>
+    <![endif]-->
+
+    </head>
+
+    <body>
+        <header class="gradient">
+
+    <div class="container-fluid">
+
+        <a class="header-logo" href="/"></a>
+
+        <div class="header-directory-options">
+        
+
+
+        </div><!-- .header-directory-options -->
+
+        <div class="header-account-functions">
+
+            <ul>
+                
+                    <li class="header-account-functions-login"><a href="http://directory.crittendenonline.com/accounts/login/">Login</a></li>
+                
+            </ul>
+
+        </div><!-- .header-account-functions -->
+
+    </div><!-- .container-fluid -->
+
+</header>
+
+        
+
+        
+
+
+<div class="container-fluid content-area" role="main">
+
+  <div class="row-fluid center">
+
+    <div class="span12">
+      
+      <div class="container-fluid content-area login" role="main">
+        <div class="row-fluid center">
+          <div class="span12">
+
+            <h1 class="login">Get Free Access</h1>
+            
+
+            <div class="login-box login-or-join center">
+
+              <script type="text/javascript">var submitted=false;</script>
+              	<iframe name="hidden_iframe" id="hidden_iframe" style="display:none;"  onload="if(submitted) {window.location='confirmation.html';}"></iframe>
+              <form action="https://docs.google.com/forms/d/1nzaq42Ip0djhqkMqxnIzZISM8ucAJTLsz-0aT2Cr-D8/formResponse" method="post" id="ss-form" target="redirect" onsubmit="submitted=true;" name="ss-form">
+                
+
+                <div class="input-left">
+                  
+                  <input id="firstname" name="entry.1599794928" type="text" placeholder="First Name">
+                  <input id="lastname" name="entry.1069437311" type="text" placeholder="Last Name">
+
+                  
+                  <input id="email" name="entry.182930085" type="text" placeholder="email">
+
+                  <input type="hidden" id="location" name="entry.306861120" value="Boise, ID" />
+                  <input type="hidden" id="src_url" name="entry.646165530" value="<?php echo $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];?>" />
+                  <input type="hidden" id="ip_address" name="entry.59827904" value="<?php echo $_SERVER['REMOTE_ADDR'];?>" />
+
+
+                </div><!-- .input-left -->
+
+                <div class="input-right">
+
+                  <h3>Newsletter Archives</h3>
+
+                  <p>Get a free trial access to the latest news.</p>
+
+                  <input type="submit" value="Request Access" class="btn btn-primary btn-large submit" />
+
+                </div><!-- .input-right -->
+
+              </form>
+
+              <div class="clear">&nbsp;</div>
+
+            </div><!-- .login-box center -->
+
+          </div><!-- .span12 -->
+        </div><!-- .row-fluid -->
+      </div>
+      
+      
+
+      <div class="center login-help-options">
+
+        <p class="login-help">Help: &nbsp; <a href="http://directory.crittendenonline.com/accounts/password/reset/">Forgot my Password</a></p>
+
+        <p class="login-call">Don't have an account? Call Crittenden to get Access 1-800-421-3483</p>
+
+      </div><!-- .center login-help-options -->
+
+    </div><!-- .span12 -->
+
+  </div><!-- .row-fluid -->
+
+</div><!-- .container-fluid .content-area -->
+
+
+        <footer>
+
+    <div class="footer-top">
+
+        <div class="container-fluid">
+
+            <p>Your source for Advanced Information and In-Depth Analysis</p>
+
+        </div><!-- .container-fluid -->
+
+    </div><!-- .footer-top -->
+
+    <div class="footer-middle">
+
+        <div class="container-fluid">
+
+            <div class="footer-middle-content-left">
+
+                <p><b>Crittenden Research, Inc.</b> is a leading source of forecast and analysis on financial markets, insurance and real estate, with members nationwide. Since 1972 we have provided to our members inside information that is overlooked by advertising-driven news sources, helping our members plan strategies that keep them and their companies ahead of the competition.</p>
+
+                <p>Our editorial team includes reporters, researchers, and financial analysts. This inside information and forecasts are delivered through reports, directories, special real estate briefs, special insurance briefs and e-mails. We utilize many forms of delivery for our publications to ensure our unique analysis and insight is delivered as quickly and early as possible. Relevant contacts and other information are included with many stories.</p>
+
+            </div><!-- .footer-middle-content-left -->
+
+            <img class="footer-middle-badge" src="http://directory.crittendenonline.com/media/images/footer/footer-badge.png" alt="footer-badge">
+
+            <div class="footer-middle-right">
+
+                <p>To subscribe or inquire<br />Call: (800) 421-3483</p>
+                <p><a href="mailto:awilson@crittendenonline.com?bcc=patrick@crittendenonline.com&subject=User%20Feedback%20Regarding%20Insurance%20Program%20Directory." target="_blank">Contact Editor</a></p>
+
+            </div><!-- .footer-middle-right -->
+
+        </div><!-- .container-fluid -->
+
+    </div><!-- .footer-middle -->
+
+    <div class="footer-bottom">
+
+        <div class="container-fluid">
+
+            <p class="pull-left">Copyright &copy; 2012 Crittenden.com All Rights Reserved.</p>
+            <p class="pull-right">Developed by <a href="http://www.smashbrand.com" target="_blank">SmashBrand</a></p>
+        </div><!-- .container-fluid -->
+
+    </div><!-- .footer-bottom -->
+
+</footer>
+
+        <!-- Javascript
+================================================== -->
+<script src="http://directory.crittendenonline.com/media/twitter-bootstrap/js/jquery.js"></script>
+<script src="http://directory.crittendenonline.com/media/twitter-bootstrap/js/bootstrap-dropdown.js"></script>
+<script src="http://directory.crittendenonline.com/media/twitter-bootstrap/js/bootstrap-modal.js"></script>
+<script src="http://directory.crittendenonline.com/media/js/subscriber/chosen/chosen/chosen.jquery.js"></script>
+<script src="http://directory.crittendenonline.com/media/js/jquery.placeholder.min.js"></script>
+<script src="http://directory.crittendenonline.com/media/js/subscriber/functions.js"></script>
+<script src="http://directory.crittendenonline.com/media/js/search.js"></script>
+<script src="http://directory.crittendenonline.com/media/js/secure-content.js"></script>
+<script>
+    function goBack(){
+        window.history.back();
+    }
+</script>
+
+
+        
+        	<script type="text/javascript">
+
+  var current_dir = get_current_directory();
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39613374-1']);
+  _gaq.push(['_setDomainName', 'crittenden-dev.smashbrand.com']);
+  _gaq.push(['_trackPageview']);
+  _gaq.push(['_setCustomVar', 1, 'User Role', '', 1 ]); // Sets a visitor-level (1) variable in slot #1
+
+  if(current_dir){
+    _gaq.push(['_setCustomVar', 2, 'Directory', current_dir, 3 ]);
+  }
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+  function get_current_directory(){
+    var url_array = window.location.href.split("/");
+
+
+    if(searchStringInArray("admin", url_array) == -1){
+        // do not record admin pages
+        return false;
+    }
+
+    else{
+        if(searchStringInArray ("retail-space-users", url_array)){
+            return "Retail Space Users";
+        }
+        else if(searchStringInArray ("real-estate-pros", url_array)){
+            return "Real Estate Pros";
+        }
+        else if(searchStringInArray ("insurance", url_array)){
+            return "Insurance";
+        }
+    }
+
+  }
+
+  function searchStringInArray (str, strArray) {
+    for (var j=0; j<strArray.length; j++) {
+        if (strArray[j].match(str)) return true;
+    }
+    return false;
+  }
+</script>
+        
+
+ 
+  <script src="http://directory.crittendenonline.com/media/js/login_page.js" type="text/javascript"></script>
+
+
+    </body>
+</html>
